0e0bf3f5a3d548006baad448c45a45f678f2be75
Ba Thien Le <bathien.le.pro@gmail.com>
2024-10-29 11:39:40 +0100

    style(lint): fix code lint (#10)

--- tests/test_wsidicom.py
+++ tests/test_wsidicom.py
@@ -1,22 +1,25 @@
-import os, io
-import pytest
-from datetime import datetime
-from PIL import Image
-from fastapi import APIRouter
-import urllib.request
-from pims.formats import FORMATS
-from pims.importer.importer import FileImporter
-from pims.files.file import (ORIGINAL_STEM, Path, SPATIAL_STEM, HISTOGRAM_STEM)
-from pims.api.utils.models import HistogramType
-from pims.processing.histograms.utils import build_histogram_file
-from pims.formats.utils.factories import FormatFactory
-from pims.utils.types import parse_float
-from pims.files.archive import Archive
+"""WSI Dicom image tests."""
 
+import io
+import os
+import urllib.request
+from datetime import datetime
+
+import pytest
+from PIL import Image
+from pims.api.utils.models import HistogramType
+from pims.files.archive import Archive
+from pims.files.image import Image as FileImage
+from pims.files.file import HISTOGRAM_STEM, ORIGINAL_STEM, SPATIAL_STEM, Path
+from pims.formats.utils.factories import FormatFactory
+from pims.importer.importer import FileImporter
+from pims.processing.histograms.utils import build_histogram_file
+from pims.utils.types import parse_float
 from wsidicom import WsiDicom
 
+
 def get_image(path, filename):
-    filepath = os.path.join(path,filename)
+    filepath = os.path.join(path, filename)
     # If image does not exist locally -> download image
     if not os.path.exists(path):
         os.mkdir("/data/pims/upload_test_wsidicom")
@@ -46,7 +49,7 @@ def get_image(path, filename):
         try:
             fi.upload_path = Path(filepath)
             original_filename = Path(f"{ORIGINAL_STEM}.WSIDICOM")
-            fi.original_path = fi.processed_dir / original_filename 
+            fi.original_path = fi.processed_dir / original_filename
             archive = Archive.from_path(fi.upload_path)
             archive.extract(fi.original_path)
             new_original_path = fi.processed_dir / original_filename
@@ -59,7 +62,7 @@ def get_image(path, filename):
         except Exception as e:
             print("Importation of images could not be done")
             print(e)
-            
+
     if not os.path.exists(os.path.join(path, "processed/histogram")):
         if os.path.exists(os.path.join(path, "processed")):
             fi = FileImporter(f"/data/pims/upload_test_wsidicom/{filename}")
@@ -68,156 +71,217 @@ def get_image(path, filename):
             original_filename = Path(f"{ORIGINAL_STEM}.WSIDICOM")
             fi.original_path = fi.processed_dir / original_filename
         try:
-            from pims.files.image import Image
-            fi.histogram_path = fi.processed_dir/Path(HISTOGRAM_STEM)
+            fi.histogram_path = fi.processed_dir / Path(HISTOGRAM_STEM)
             format = FormatFactory().match(fi.original_path)
-            fi.original = Image(fi.original_path, format=format)
-            fi.histogram = build_histogram_file(fi.original, fi.histogram_path, HistogramType.FAST)
+            fi.original = FileImage(fi.original_path, format=format)
+            fi.histogram = build_histogram_file(
+                fi.original,
+                fi.histogram_path,
+                HistogramType.FAST,
+            )
         except Exception as e:
             print("Creation of histogram representation could not be done")
             print(e)
 
+
 def dictify(ds):
-    output = dict()
+    output = {}
     for elem in ds:
-        if elem.VR != 'SQ':
+        if elem.VR != "SQ":
             output[elem.name] = elem.value
         else:
             output[elem.name] = [dictify(item) for item in elem]
     return output
 
+
 def parse_acquisition_date(date: str):
-        """
-        Datetime examples: 20211216163400 -> 16/12/21, 16h34
-        """
-        try:
-            if date:
-                str_date = datetime.strptime(date.split('.')[0], "%Y%m%d%H%M%S")
-                return f'{str_date}'
-                
-            else:
-                return None
-        except (ValueError, TypeError):
-            return None
-    
+    """
+    Datetime examples: 20211216163400 -> 16/12/21, 16h34
+    """
+    try:
+        if date:
+            str_date = datetime.strptime(date.split(".")[0], "%Y%m%d%H%M%S")
+            return f"{str_date}"
+
+        return None
+    except (ValueError, TypeError):
+        return None
+
+
 def test_wsidicom_exists(image_path_wsidicom):
-	# Test if the file exists, either locally either with the OAC
-	path, filename = image_path_wsidicom
-	get_image(path, filename)
-	assert os.path.exists(os.path.join(path,filename)) == True
+    # Test if the file exists, either locally either with the OAC
+    path, filename = image_path_wsidicom
+    get_image(path, filename)
+    assert os.path.exists(os.path.join(path, filename)) is True
+
 
 def test_wsidicom_info(client, image_path_wsidicom):
-    path, filename = image_path_wsidicom
-    response = client.get(f'/image/upload_test_wsidicom/{filename}/info')
+    _, filename = image_path_wsidicom
+    response = client.get(f"/image/upload_test_wsidicom/{filename}/info")
     assert response.status_code == 200
-    assert "wsidicom" in response.json()['image']['original_format'].lower()
-    wsidicom_object = WsiDicom.open(f"/data/pims/upload_test_wsidicom/processed/original.WSIDICOM/{os.path.splitext(filename)[0]}")
-    
-    assert response.json()['image']['width'] == wsidicom_object.levels.base_level.size.width
-    assert response.json()['image']['height'] == wsidicom_object.levels.base_level.size.height
+    assert "wsidicom" in response.json()["image"]["original_format"].lower()
+
+    data = response.json()
+    filepath = "/data/pims/upload_test_wsidicom/processed/original.WSIDICOM/"
+    filepath += os.path.splitext(filename)[0]
+    wsidicom_object = WsiDicom.open(filepath)
+
+    width = wsidicom_object.levels.base_level.size.width
+    assert data["image"]["width"] == width
+
+    height = wsidicom_object.levels.base_level.size.height
+    assert data["image"]["height"] == height
+
     metadata = dictify(wsidicom_object.levels.groups[0].datasets[0])
-    if 'Bits Stored' in metadata:
-        assert response.json()['image']['significant_bits'] == metadata["Bits Stored"]
+    if "Bits Stored" in metadata:
+        assert data["image"]["significant_bits"] == metadata["Bits Stored"]
     else:
-        assert response.json()['image']['significant_bits'] == 8
-    
-    assert response.json()['image']['physical_size_x'] == wsidicom_object.levels.groups[0].mpp.width
-    assert response.json()['image']['physical_size_y'] == wsidicom_object.levels.groups[0].mpp.height
-    
-    if 'Objective Lens Power' in metadata['Optical Path Sequence'][0]:
-        assert response.json()['instrument']['objective']['nominal_magnification'] == parse_float(metadata['Optical Path Sequence'][0]['Objective Lens Power'])
-	
-    if 'Acquisition DateTime' in metadata:
-        assert response.json()['image']['acquired_at'].replace("T", " ") == parse_acquisition_date(metadata['Acquisition DateTime'])
-            
+        assert data["image"]["significant_bits"] == 8
+
+    width = wsidicom_object.levels.groups[0].mpp.width
+    assert data["image"]["physical_size_x"] == width
+
+    height = wsidicom_object.levels.groups[0].mpp.height
+    assert data["image"]["physical_size_y"] == height
+
+    power = "Objective Lens Power"
+    if power in metadata["Optical Path Sequence"][0]:
+        magnification = data["instrument"]["objective"]["nominal_magnification"]
+        lens_power = parse_float(metadata["Optical Path Sequence"][0][power])
+        assert magnification == lens_power
+
+    if "Acquisition DateTime" in metadata:
+        time = data["image"]["acquired_at"].replace("T", " ")
+        assert time == parse_acquisition_date(metadata["Acquisition DateTime"])
+
+
 def test_wsidicom_associated(client, image_path_wsidicom):
-    path, filename = image_path_wsidicom
-    response = client.get(f'/image/upload_test_wsidicom/{filename}/info')
-    wsidicom_object = WsiDicom.open(f"/data/pims/upload_test_wsidicom/processed/original.WSIDICOM/{os.path.splitext(filename)[0]}")
-    
+    _, filename = image_path_wsidicom
+    response = client.get(f"/image/upload_test_wsidicom/{filename}/info")
+    filepath = "/data/pims/upload_test_wsidicom/processed/original.WSIDICOM/"
+    filepath += os.path.splitext(filename)[0]
+    wsidicom_object = WsiDicom.open(filepath)
+
     # assume there is no associated thumbnail for now
     # idx = index of the associated image in the response dictionary
     if wsidicom_object.labels:
         idx = 0
         label = wsidicom_object.read_label()
-        assert response.json()['associated'][idx]['name'] == "label"
-        assert response.json()['associated'][idx]['width'] == label.width
-        assert response.json()['associated'][idx]['height'] == label.height
-        
+        assert response.json()["associated"][idx]["name"] == "label"
+        assert response.json()["associated"][idx]["width"] == label.width
+        assert response.json()["associated"][idx]["height"] == label.height
+
     if wsidicom_object.overviews:
         if wsidicom_object.labels:
             idx = 1
         else:
             idx = 0
         macro = wsidicom_object.read_overview()
-        assert response.json()['associated'][idx]['name'] == "macro"
-        assert response.json()['associated'][idx]['width'] == macro.width
-        assert response.json()['associated'][idx]['height'] == macro.height
-        
+        assert response.json()["associated"][idx]["name"] == "macro"
+        assert response.json()["associated"][idx]["width"] == macro.width
+        assert response.json()["associated"][idx]["height"] == macro.height
+
+
 def test_wsidicom_norm_tile(client, image_path_wsidicom):
     _, filename = image_path_wsidicom
-    response = client.get(f"/image/upload_test_wsidicom/{filename}/normalized-tile/level/0/ti/0", headers={"accept": "image/jpeg"})
+    response = client.get(
+        f"/image/upload_test_wsidicom/{filename}/normalized-tile/level/0/ti/0",
+        headers={"accept": "image/jpeg"},
+    )
     assert response.status_code == 200
 
     img_response = Image.open(io.BytesIO(response.content))
     width_resp, height_resp = img_response.size
     assert width_resp == 256
     assert height_resp == 256
-	
+
 
 def test_wsidicom_thumb(client, image_path_wsidicom):
     _, filename = image_path_wsidicom
-    response = client.get(f"/image/upload_test_wsidicom/{filename}/thumb", headers={"accept": "image/jpeg"})
-    assert response.status_code == 200
-    
-    im_resp = Image.open(io.BytesIO(response.content))
-    width_resp, height_resp = im_resp.size
-    assert width_resp == 256 or height_resp == 256
-	
-def test_wsidicom_macro(client, image_path_wsidicom):
-    path, filename = image_path_wsidicom
-    response = client.get(f"/image/upload_test_wsidicom/{filename}/associated/macro", headers={"accept": "image/jpeg"})
-    assert response.status_code == 200
-    
-    im_resp = Image.open(io.BytesIO(response.content))
-    width_resp, height_resp = im_resp.size
-    assert width_resp == 256 or height_resp == 256
-    
-@pytest.mark.skip(reason="There is no label image associated with this WSI Dicom image")    
-def test_wsidicom_label(client, image_path_wsidicom):
-    path, filename = image_path_wsidicom
-    response = client.get(f"/image/upload_test_wsidicom/{filename}/associated/label", headers={"accept": "image/jpeg"})
-    assert response.status_code == 200
-    
-    im_resp = Image.open(io.BytesIO(response.content))
-    width_resp, height_resp = im_resp.size
-    assert width_resp == 256 or height_resp == 256
-    
-def test_wsidicom_resized(client, image_path_wsidicom):
-    _, filename = image_path_wsidicom
-    response = client.get(f"/image/upload_test_wsidicom/{filename}/resized", headers={"accept": "image/jpeg"})
+    response = client.get(
+        f"/image/upload_test_wsidicom/{filename}/thumb",
+        headers={"accept": "image/jpeg"},
+    )
     assert response.status_code == 200
 
     im_resp = Image.open(io.BytesIO(response.content))
     width_resp, height_resp = im_resp.size
     assert width_resp == 256 or height_resp == 256
+
+
+def test_wsidicom_macro(client, image_path_wsidicom):
+    _, filename = image_path_wsidicom
+    response = client.get(
+        f"/image/upload_test_wsidicom/{filename}/associated/macro",
+        headers={"accept": "image/jpeg"},
+    )
+    assert response.status_code == 200
+
+    im_resp = Image.open(io.BytesIO(response.content))
+    width_resp, height_resp = im_resp.size
+    assert width_resp == 256 or height_resp == 256
+
+
+@pytest.mark.skip(reason="There is no label image associated with this WSI Dicom image")
+def test_wsidicom_label(client, image_path_wsidicom):
+    _, filename = image_path_wsidicom
+    response = client.get(
+        f"/image/upload_test_wsidicom/{filename}/associated/label",
+        headers={"accept": "image/jpeg"},
+    )
+    assert response.status_code == 200
+
+    im_resp = Image.open(io.BytesIO(response.content))
+    width_resp, height_resp = im_resp.size
+    assert width_resp == 256 or height_resp == 256
+
+
+def test_wsidicom_resized(client, image_path_wsidicom):
+    _, filename = image_path_wsidicom
+    response = client.get(
+        f"/image/upload_test_wsidicom/{filename}/resized",
+        headers={"accept": "image/jpeg"},
+    )
+    assert response.status_code == 200
+
+    im_resp = Image.open(io.BytesIO(response.content))
+    width_resp, height_resp = im_resp.size
+    assert width_resp == 256 or height_resp == 256
+
 
 def test_wsidicom_mask(client, image_path_wsidicom):
     _, filename = image_path_wsidicom
-    response = client.post(f"/image/upload_test_wsidicom/{filename}/annotation/mask", headers={"accept": "image/jpeg"}, json={"annotations":[{"geometry": "POINT(10 10)"}], "height":50, "width":50})
+    response = client.post(
+        f"/image/upload_test_wsidicom/{filename}/annotation/mask",
+        headers={"accept": "image/jpeg"},
+        json={"annotations": [{"geometry": "POINT(10 10)"}], "height": 50, "width": 50},
+    )
     assert response.status_code == 200
+
 
 def test_wsidicom_crop(client, image_path_wsidicom):
     _, filename = image_path_wsidicom
-    response = client.post(f"/image/upload_test_wsidicom/{filename}/annotation/crop", headers={"accept": "image/jpeg"}, json={"annotations":[{"geometry": "POINT(10 10)"}], "height":50, "width":50})
+    response = client.post(
+        f"/image/upload_test_wsidicom/{filename}/annotation/crop",
+        headers={"accept": "image/jpeg"},
+        json={"annotations": [{"geometry": "POINT(10 10)"}], "height": 50, "width": 50},
+    )
     assert response.status_code == 200
-    
+
+
 def test_wsidicom_histogram_perimage(client, image_path_wsidicom):
     _, filename = image_path_wsidicom
-    response = client.get(f"/image/upload_test_wsidicom/{filename}/histogram/per-image", headers={"accept": "image/jpeg"})
+    response = client.get(
+        f"/image/upload_test_wsidicom/{filename}/histogram/per-image",
+        headers={"accept": "image/jpeg"},
+    )
     assert response.status_code == 200
-    
+
+
 def test_wsidicom_annotation(client, image_path_wsidicom):
     _, filename = image_path_wsidicom
-    response = client.get(f"/image/upload_test_wsidicom/{filename}/metadata/annotations", headers={"accept": "image/jpeg"})
+    response = client.get(
+        f"/image/upload_test_wsidicom/{filename}/metadata/annotations",
+        headers={"accept": "image/jpeg"},
+    )
     assert response.status_code == 200
