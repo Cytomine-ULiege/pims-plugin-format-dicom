92885a5a4f45d1ce9ca166c9de078d3160aeed9c
Ba Thien Le <bathien.le.pro@gmail.com>
2024-10-29 10:52:23 +0100

    style: format dicom file

--- pims_plugin_format_dicom/dicom.py
+++ pims_plugin_format_dicom/dicom.py
@@ -1,8 +1,10 @@
+"""Classes and functions to handle WSI DICOM format."""
+
 import os
 from datetime import datetime
 from functools import cached_property
 from pathlib import Path
-from typing import List, Optional
+from typing import Any, Dict, List, Optional
 
 import shapely
 from pydicom.multival import MultiValue
@@ -10,7 +12,14 @@ from wsidicom.graphical_annotations import Point as WsiPoint
 from wsidicom.graphical_annotations import Polygon as WsiPolygon
 from wsidicom.wsidicom import WsiDicom
 
-from pims.formats.utils.abstract import AbstractChecker, AbstractParser, AbstractReader, AbstractFormat, CachedDataPath
+from pims.formats.utils.abstract import (
+    AbstractChecker,
+    AbstractParser,
+    AbstractReader,
+    AbstractFormat,
+    CachedDataPath,
+)
+from pims.files.file import Path as FilePath
 from pims.formats.utils.histogram import DefaultHistogramReader
 from pims.formats.utils.structures.annotations import ParsedMetadataAnnotation
 from pims.formats.utils.structures.metadata import ImageMetadata, ImageChannel
@@ -20,38 +29,45 @@ from pims.utils.dtypes import np_dtype
 from pims.utils.types import parse_float
 
 
-def dictify(ds):
-    output = dict()
+def dictify(ds: List[Any]) -> Dict[str, Any]:
+    """Convert a list to a dictionary."""
+
+    output = {}
     for elem in ds:
-        if elem.VR != 'SQ':
+        if elem.VR != "SQ":
             output[elem.name] = elem.value
         else:
             output[elem.name] = [dictify(item) for item in elem]
+
     return output
 
 
-def recurse_if_SQ(ds):
+def recurse_if_SQ(ds: List[Any]) -> List[Any]:
+    """Recursively iterate over the data elements."""
+
     list_ds = []
 
     for data_element in ds:
-        if data_element.VR != 'SQ':
+        if data_element.VR != "SQ":
             list_ds.append(data_element)
 
         else:
             for elmt in data_element:
                 list_recursive = recurse_if_SQ(elmt)
                 list_ds.extend(list_recursive)
+
     return list_ds
 
 
 def cached_wsi_dicom_file(format: AbstractFormat) -> WsiDicom:
-    return format.get_cached('_wsi_dicom', WsiDicom.open, str(format.path))
+    """Get the WSI DICOM object from the cache."""
+    return format.get_cached("_wsi_dicom", WsiDicom.open, str(format.path))
 
 
 def get_root_file(path: Path) -> Optional[Path]:
     """Try to get WSI DICOM directory (as it is a multi-file format)."""
     if path.is_dir():
-        if sum(1 for _ in Path(path).glob('*')):
+        if sum(1 for _ in Path(path).glob("*")):
             for child in path.iterdir():
                 if child.is_dir():
                     return path
@@ -63,21 +79,23 @@ class WSIDicomChecker(AbstractChecker):
 
     @classmethod
     def match(cls, pathlike: CachedDataPath) -> bool:
-        from pims.files.file import Path
+
         path = pathlike.path
         if os.path.isdir(path):
             # list_subdir = [f.path for f in os.scandir(path) if os.path.isdir(f)]
             # if len(list_subdir) == 1:
             for child in os.listdir(path):
                 # verification on the format signature for each .dcm file
-                complete_path = Path(os.path.join(path, child))
+                complete_path = FilePath(os.path.join(path, child))
                 cached_child = CachedDataPath(complete_path)
-                buf = cached_child.get_cached('signature', cached_child.path.signature)
-                if not (len(buf) > cls.OFFSET + 4 and
-                        buf[cls.OFFSET] == 0x44 and
-                        buf[cls.OFFSET + 1] == 0x49 and
-                        buf[cls.OFFSET + 2] == 0x43 and
-                        buf[cls.OFFSET + 3] == 0x4D):
+                buf = cached_child.get_cached("signature", cached_child.path.signature)
+                if not (
+                    len(buf) > cls.OFFSET + 4
+                    and buf[cls.OFFSET] == 0x44
+                    and buf[cls.OFFSET + 1] == 0x49
+                    and buf[cls.OFFSET + 2] == 0x43
+                    and buf[cls.OFFSET + 3] == 0x4D
+                ):
                     return False
             return True
             # return False
@@ -94,7 +112,7 @@ class WSIDicomParser(AbstractParser):
         imd.width = levels.base_level.size.width
         imd.height = levels.base_level.size.height
         metadata = dictify(wsidicom_object.levels.groups[0].datasets[0])
-        if 'Bits Stored' in metadata:
+        if "Bits Stored" in metadata:
             imd.significant_bits = metadata["Bits Stored"]
         else:
             imd.significant_bits = 8
@@ -108,16 +126,17 @@ class WSIDicomParser(AbstractParser):
         if "Manufacturer's Model Name" in metadata:
             imd.microscope.model = metadata["Manufacturer's Model Name"]
 
-        if 'Objective Lens Power' in metadata['Optical Path Sequence'][0]:
+        if "Objective Lens Power" in metadata["Optical Path Sequence"][0]:
             imd.objective.nominal_magnification = parse_float(
-                metadata['Optical Path Sequence'][0]['Objective Lens Power'])
+                metadata["Optical Path Sequence"][0]["Objective Lens Power"]
+            )
 
         if imd.n_channels == 3:
-            imd.set_channel(ImageChannel(index=0, suggested_name='R'))
-            imd.set_channel(ImageChannel(index=1, suggested_name='G'))
-            imd.set_channel(ImageChannel(index=2, suggested_name='B'))
+            imd.set_channel(ImageChannel(index=0, suggested_name="R"))
+            imd.set_channel(ImageChannel(index=1, suggested_name="G"))
+            imd.set_channel(ImageChannel(index=2, suggested_name="B"))
         else:
-            imd.set_channel(ImageChannel(index=0, suggested_name='L'))
+            imd.set_channel(ImageChannel(index=0, suggested_name="L"))
         imd.n_channels_per_read = imd.n_channels
 
         if wsidicom_object.labels:
@@ -140,13 +159,22 @@ class WSIDicomParser(AbstractParser):
 
         metadata = dictify(wsidicom_object.levels.groups[0].datasets[0])
         imd = super().parse_known_metadata()
-        imd.physical_size_x = wsidicom_object.levels.groups[0].mpp.width * UNIT_REGISTRY("micrometers")
-        imd.physical_size_y = wsidicom_object.levels.groups[0].mpp.height * UNIT_REGISTRY("micrometers")
+        imd.physical_size_x = wsidicom_object.levels.groups[
+            0
+        ].mpp.width * UNIT_REGISTRY("micrometers")
+        imd.physical_size_y = wsidicom_object.levels.groups[
+            0
+        ].mpp.height * UNIT_REGISTRY("micrometers")
 
         imd.physical_size_z = self.parse_physical_size(
-            metadata['Shared Functional Groups Sequence'][0]['Pixel Measures Sequence'][0]['Spacing Between Slices'])
-        if 'Acquisition DateTime' in metadata:
-            imd.acquisition_datetime = self.parse_acquisition_date(metadata['Acquisition DateTime'])
+            metadata["Shared Functional Groups Sequence"][0]["Pixel Measures Sequence"][
+                0
+            ]["Spacing Between Slices"]
+        )
+        if "Acquisition DateTime" in metadata:
+            imd.acquisition_datetime = self.parse_acquisition_date(
+                metadata["Acquisition DateTime"]
+            )
         return imd
 
     def parse_raw_metadata(self):
@@ -161,10 +189,10 @@ class WSIDicomParser(AbstractParser):
             if data_element.is_private:
                 tag = data_element.tag
                 name = f"{tag.group:04x}_{tag.element:04x}"  # noqa
-            name = name.replace(' ', '')
+            name = name.replace(" ", "")
 
             value = data_element.value
-            if type(value) is MultiValue:
+            if isinstance(value, MultiValue):
                 value = list(value)
             store.set(name, value, namespace="DICOM")
 
@@ -180,7 +208,9 @@ class WSIDicomParser(AbstractParser):
             level_info = wsidicom_object.levels.get_level(level)
             level_size = level_info.size
             tile_size = level_info.tile_size
-            pyramid.insert_tier(level_size.width, level_size.height, (tile_size.width, tile_size.height))
+            pyramid.insert_tier(
+                level_size.width, level_size.height, (tile_size.width, tile_size.height)
+            )
 
         return pyramid
 
@@ -217,11 +247,10 @@ class WSIDicomParser(AbstractParser):
         """
         try:
             if date:
-                str_date = datetime.strptime(date.split('.')[0], "%Y%m%d%H%M%S")
-                return f'{str_date}'
+                str_date = datetime.strptime(date.split(".")[0], "%Y%m%d%H%M%S")
+                return f"{str_date}"
 
-            else:
-                return None
+            return None
         except (ValueError, TypeError):
             return None
 
@@ -234,7 +263,9 @@ class WSIDicomParser(AbstractParser):
 
 class WSIDicomReader(AbstractReader):
 
-    def read_thumb(self, out_width, out_height, precomputed=True, c=None, z=None, t=None):
+    def read_thumb(
+        self, out_width, out_height, precomputed=True, c=None, z=None, t=None
+    ):
         img = cached_wsi_dicom_file(self.format)
 
         return img.read_thumbnail((out_width, out_height))
@@ -242,11 +273,18 @@ class WSIDicomReader(AbstractReader):
     def read_window(self, region, out_width, out_height, c=None, z=None, t=None):
         img = cached_wsi_dicom_file(self.format)
 
-        tier = self.format.pyramid.most_appropriate_tier(region, (out_width, out_height))
+        tier = self.format.pyramid.most_appropriate_tier(
+            region,
+            (out_width, out_height),
+        )
         region = region.scale_to_tier(tier)
         level = tier.level
         norm_level = img.levels.levels[level]
-        return img.read_region((region.left, region.top), norm_level, (region.width, region.height))
+        return img.read_region(
+            (region.left, region.top),
+            norm_level,
+            (region.width, region.height),
+        )
 
     def read_tile(self, tile, c=None, z=None, t=None):
         return self.read_window(tile, tile.width, tile.height, c, z, t)
